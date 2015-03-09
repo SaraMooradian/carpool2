62f2d93a7e29cb8939837f43c98d1d506772207e
Sara Mooradian <sara.mooradian@gmail.com>
2015-03-09 15:50:33 -0700

    Corrected controller from last migration dont forget persmissions

--- app/models/carpool.rb
+++ app/models/carpool.rb
@@ -2,7 +2,9 @@ class Carpool < ActiveRecord::Base
 	belongs_to :user
 	has_many :starttimes
 	has_many :endtimes
+	has_many :startdates
+	has_many :enddates
 	
-	validates :description, :starttime, :endtime, presence: true
+	validates :description, :starttime, :endtime, :startdate, :enddate, presence: true
 
 end
